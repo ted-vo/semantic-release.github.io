20dc4cd7689014210d120bb915b5c4689ea2ccb1
Christoph Witzko <github@christophwitzko.com>
2020-08-20 12:25:10 +0200

    feat: create basic index file

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path"
 )
 
 func checkError(err error) {
@@ -16,4 +18,7 @@ func main() {
 	fmt.Printf("creating %s\n", dest)
 	checkError(os.RemoveAll("./plugin-index"))
 	checkError(os.MkdirAll(dest, 0755))
+	plugJson := path.Join(dest, "plugins.json")
+	fmt.Printf("creating %s\n", plugJson)
+	checkError(ioutil.WriteFile(plugJson, []byte("{}"), 0755))
 }
