0b7d60caa380e0a1aefe10d397bc87d213fb8f97
Christoph Witzko <github@christophwitzko.com>
2021-07-27 21:47:48 +0200

    fix: os arch regexp

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ import (
 	"golang.org/x/oauth2"
 )
 
-var osArchRe = regexp.MustCompile("(?i)(aix|android|darwin|dragonfly|freebsd|hurd|illumos|js|linux|nacl|netbsd|openbsd|plan9|solaris|windows|zos)(_|-)(386|amd64|amd64p32|arm|armbe|arm64|arm64be|ppc64|ppc64le|mips|mipsle|mips64|mips64le|mips64p32|mips64p32le|ppc|riscv|riscv64|s390|s390x|sparc|sparc64|wasm)")
+var osArchRe = regexp.MustCompile(`(?i)(aix|android|darwin|dragonfly|freebsd|hurd|illumos|js|linux|nacl|netbsd|openbsd|plan9|solaris|windows|zos)(_|-)(386|amd64|amd64p32|arm|armbe|arm64|arm64be|ppc64|ppc64le|mips|mipsle|mips64|mips64le|mips64p32|mips64p32le|ppc|riscv|riscv64|s390|s390x|sparc|sparc64|wasm)(\.exe)?$`)
 
 func checkError(err error) {
 	if err != nil {
